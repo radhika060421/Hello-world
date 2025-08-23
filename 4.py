6c20dbccf8ff4329b7ebca256a6a092b05ac2d1c
radhika060421 <radhupatel0604@gmail.com>
2025-08-23 22:03:20 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,6 @@
+# write a python program to add two numbers using command line arguments...
+
+first_number = int(input("Enter first number :"))
+second_number = int(input("Enter second number :"))
+sum = first_number + second_number
+print(f"The sum of {first_number} and {second_number} is {sum}")
